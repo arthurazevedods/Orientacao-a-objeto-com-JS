d6777113496493628dbf437734ec86cdfd84a65f
arthur <arthurazevedods@gmail.com>
2024-03-09 12:14:35 -0300

    aula02

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 class Cliente {
-    constructor(nome, cpf, agencia, saldo) {
+    constructor(nome, cpf, contacorrente) {
         this.nome = nome;
         this.cpf = cpf;
+        this.contacorrente = contacorrente
+    }
+}
+
+class ContaCorrente{
+    constructor(agencia, saldo){
         this.agencia = agencia;
         this.saldo = saldo;
     }
 }
 
-const cliente1 = new Cliente("Zuko", "600.000.000-70", "4444-0", 200.50);
-const cliente2 = new Cliente("Azula", "610.000.000-11", "4444-0", 2200.00);
-const cliente3 = new Cliente("Aaag", "510.000.000-00", "4233-0", 0.80);
+const cliente1 = new Cliente("Zuko", "600.000.000-70", new ContaCorrente("4444-0", 23.00));
+const cliente2 = new Cliente("Azula", "610.000.000-11", new ContaCorrente("4444-0", 1540.00));
+const cliente3 = new Cliente("Aaag", "510.000.000-00", new ContaCorrente("4233-0", 0.80));
 
 const clientes = [cliente1, cliente2, cliente3];
 
@@ -18,8 +24,8 @@ async function percorrerClientes(array) {
         await delay(5000); // Aguardar 5 segundos antes de continuar
         console.log("Nome: " + cliente.nome);
         console.log("CPF: " + cliente.cpf);
-        console.log("Agência: " + cliente.agencia);
-        console.log("Saldo: R$" + cliente.saldo);
+        console.log("Agência: " + cliente.contacorrente.agencia);
+        console.log("Saldo: R$" + cliente.contacorrente.saldo);
         console.log("-----------------------");
     }
 }
@@ -29,4 +35,5 @@ function delay(ms) {
     console.log("Carregando informações...")
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
 percorrerClientes(clientes);
